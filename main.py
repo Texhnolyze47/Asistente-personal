684043bcdb3404ff629a5e28b990815248dce3a3
Texhnolyze47 <ichigobankai136@gmail.com>
2023-01-12 15:27:53 -0600

    Se quitaron librerias incecesarias

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 import datetime
-import io
-
-import pygame
 import wikipedia
-from pydub import AudioSegment
 import speech_recognition as sr
-import whisper
-import tempfile
-import os
 import pyttsx3
 import pywhatkit
 
@@ -36,7 +29,7 @@ def listen():
             commands = listener.recognize_google(voice, language='es-MX')
             commands = ''.join(commands).lower()
             if name in commands:
-                commands = commands.replace('alexa','')
+                commands = commands.replace('alexa', '')
                 print(commands)
             else:
                 talk("Por favor di mi nombre")
@@ -54,8 +47,8 @@ def main():
                 talk(f"Reproduciendo {music}")
                 pywhatkit.playonyt(music)
             elif 'busca' in response:
-                order = response.replace('busca','')
-                info = wikipedia.summary(order,1)
+                order = response.replace('busca', '')
+                info = wikipedia.summary(order, 1)
                 talk(info)
             elif 'hora' in response:
                 hora = datetime.datetime.now().strftime("%I:%M %p")
@@ -66,6 +59,7 @@ def main():
     except Exception as e:
         talk(f"Lo siento no te entendi debido a este error")
         print(e)
+
 
 if __name__ == '__main__':
     main()
